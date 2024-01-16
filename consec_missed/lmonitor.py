e24d37b8b2aefec83647d2c813b85485062beb68
Davide Gessa (dakk) <gessadavide@gmail.com>
2024-01-16 09:59:28 +0100

    update lmonitor

--- consec_missed/lmonitor.py
+++ consec_missed/lmonitor.py
@@ -19,13 +19,20 @@ conf = json.loads(f.read())
 prevConsec = 0
 
 def alert(cons):
+    if cons > 0:
+        text = f"Consecutive missed block increased to {cons}"
+    else:
+        text = f"Missed blocks come back to zero"
     d = requests.get('https://api.telegram.org/bot%s/sendMessage?text=%s&chat_id=%s' %
-						 (conf['telegramApi'], f"Consecutive missed block increased to {cons}", conf['chatId'])).json()
+						 (conf['telegramApi'], text, conf['chatId'])).json()
 
 while True:
+    print ('monitor running')
     r = requests.get(API + EP + conf['address']).json()['data'][0]
     if r['consecutiveMissedBlocks'] > prevConsec:
         alert(r['consecutiveMissedBlocks'])
+    if r['consecutiveMissedBlocks'] == 0 and prevConsec > 0:
+        alert(0)
     
     prevConsec = r['consecutiveMissedBlocks']
         
